95f36d20bfce5275108a8fb296268b843396b1ff
Exelsi0R <157590473+AlfaExelsior@users.noreply.github.com>
2024-05-16 15:09:54 +0200

    Update index.js

--- src/declarations/token/index.js
+++ src/declarations/token/index.js
@@ -1,9 +1,6 @@
 import { Actor, HttpAgent } from "@dfinity/agent";
-
-// Imports and re-exports candid interface
 import { idlFactory } from './token.did.js';
 export { idlFactory } from './token.did.js';
-// CANISTER_ID is replaced by webpack based on node environment
 export const canisterId = process.env.TOKEN_CANISTER_ID;
 
 /**
